eb1ed4260366c8ce508e958f69cbaad3a2052878
Kaushik Nagarajan <kaushik@tamu.edu>
2025-03-03 15:52:11 -0600

    worked on getting song titles from spotify API

--- dataset/getSongTitles.py
+++ dataset/getSongTitles.py
@@ -1,4 +1,25 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv, dotenv_values
+import os
 
 # Set up spotify API credentials
+load_dotenv()
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
+
+sp_oauth = SpotifyOAuth(client_id=client_id, 
+                        client_secret=client_secret,
+                        redirect_uri='http://localhost:1111',
+                        scope='user-library-read')
+
+auth_url = sp_oauth.get_authorize_url()
+print(f"Please go to this URL to authorize: {auth_url}")
+
+auth_code = input("Enter auth code: ")
+
+# Get the access token from auth code
+token_info = sp_oauth.get_access_token(auth_code)
+access_token = token_info['access_token']
+
+file = open('songs.txt', 'w')
