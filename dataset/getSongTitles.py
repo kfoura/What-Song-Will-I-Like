1e531285afda4b1674e5b492a72ed09e935b3fa6
Kaushik Nagarajan <kaushik@tamu.edu>
2025-03-03 22:12:58 -0600

    Finished script to scrape song titles from spotify playlists

--- dataset/getSongTitles.py
+++ dataset/getSongTitles.py
@@ -10,9 +10,10 @@ client_secret = os.getenv('CLIENT_SECRET')
 
 sp_oauth = SpotifyOAuth(client_id=client_id, 
                         client_secret=client_secret,
-                        redirect_uri='http://localhost:1111',
+                        redirect_uri='http://localhost:3030',
                         scope='user-library-read')
 
+# Get the authorization URL
 auth_url = sp_oauth.get_authorize_url()
 print(f"Please go to this URL to authorize: {auth_url}")
 
@@ -22,4 +23,26 @@ auth_code = input("Enter auth code: ")
 token_info = sp_oauth.get_access_token(auth_code)
 access_token = token_info['access_token']
 
-file = open('songs.txt', 'w')
+# Set up the Spotipy client with the access token
+sp = spotipy.Spotify(auth=access_token)
+
+playlist_id = input("Please input the ID of the playlist you want to load in: ")
+playlist = sp.playlist_items(playlist_id=playlist_id)
+
+
+
+tracks = playlist['items']
+
+# Pagination to get all tracks
+while playlist['next']:
+    playlist = sp.next(playlist)
+    tracks.extend(playlist['items'])
+
+file = open('./dataset/songs.txt', 'w', encoding = 'utf-8')
+
+# Print track details
+for idx, item in enumerate(tracks, start=1):
+    track = item['track']
+    file.write(f"{track['name']} - {track['artists'][0]['name']} \n")
+
+file.close()
