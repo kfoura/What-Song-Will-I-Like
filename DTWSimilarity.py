939af5ab742c6cc6c31eae317e10657de31b2a76
Kaushik Nagarajan <kaushik@tamu.edu>
2025-03-04 18:17:46 -0600

    started implementing DTW similarity

---
+++ DTWSimilarity.py
@@ -0,0 +1,25 @@
+# This algorithm uses DTW to compare MFCCs
+import librosa
+import numpy as np
+from fastdtw import fastdtw
+from scipy.spatial.distance import euclidean
+import os
+
+mfcc_list = []
+
+# load in each of the MFCC arrays
+for file in os.listdir('./dataset/normalized_mfcc_files'):
+    if file.endswith('.npy'):
+        mfcc = np.load(os.path.join('./dataset/normalized_mfcc_files', file))
+        # take the average of each array
+        mfcc_mean = np.mean(mfcc, axis=1)
+        mfcc_list.append(mfcc_mean)
+    
+total_mfcc = np.mean(mfcc, axis = 0)
+
+distance = []
+for i, mfcc in enumerate(mfcc_list):
+    # find the distance between the average MFCC and each MFCC
+    dtw_distance = fastdtw(mfcc, total_mfcc, dist=euclidean)[0]
+    distance.append(dtw_distance)
+        
